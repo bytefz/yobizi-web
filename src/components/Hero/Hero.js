99709836b568850045e9c74aebeaa871bbbeba62
Franzua Plasencia <franzua.15pc@gmail.com>
2023-06-30 03:47:34 -0500

    Change attributes

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,13 +3,13 @@ import NextLink from "next/link";
 
 export const Hero = () => {
   return (
-    <section class=" flex flex-col items-center">
-      <div class="py-8 px-4 mx-auto max-w-screen-xl flex flex-col md:flex-row lg:py-16 lg:px-12">
+    <section className=" flex flex-col items-center">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl flex flex-col md:flex-row lg:py-16 lg:px-12">
         <div className="text-center items-center">
-          <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-4xl lg:text-5xl dark:text-white">
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-4xl lg:text-5xl dark:text-white">
             ¿Listo para encontrar al mejor talento independiente?
           </h1>
-          <p class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white">
+          <p className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-4xl dark:text-white">
             Descubre nuestra aplicación hoy mismo
           </p>
 
@@ -41,9 +41,7 @@ export const Hero = () => {
       <p className="pb-8 text-white">
         Más de 300,000 personas ya se han registrado.
       </p>
-      <div>
-        <NextImage src="/users.png" width={200} height={50} />
-      </div>
+      <NextImage src="/users.png" width={200} height={50} />
     </section>
   );
 };
